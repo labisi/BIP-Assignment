6355c6a203d8f53b0c885961e19a39e772506a2d
labisi <olabisi18@gmail.com>
2019-03-31 07:31:28 +0100

    i made changes to js and index files

--- bookworm.js
+++ bookworm.js
@@ -3,15 +3,15 @@ function calculate(quantityBought, priceOfProduct){
     var qob = parseInt(quantityBought);
     var price = parseFloat(priceOfProduct);
 
-    var presentSubtotal = parseInt(document.querySelector("#subtotal").innerHTML);
+    var presentSubtotal = parseInt(document.querySelectorAll("#subtotal").innerHTML); // bisi made some changes
     
     var summa = presentSubtotal + (price * qob);
     var vat = (5*summa)/100;
     var total = summa + vat;
 
-    document.querySelector("#subtotal").innerHTML = summa;
-    document.querySelector(".vat").innerHTML = vat;
-    document.querySelector(".total").innerHTML = total;
+    document.querySelectorAll("#subtotal").innerHTML = summa; //bisi made some changes here
+    document.querySelectorAll(".vat").innerHTML = vat;//bisi made changes
+    document.querySelectorAll(".total").innerHTML = total; //bisi made changes
 }
 
 function remove(a,b,c){
